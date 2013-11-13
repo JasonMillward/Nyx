22ae1ec622db5f8bb0923816796ea0e2969c4ee6
Jason Millward <jason@jcode.me>
2013-11-13 11:10:13 +0000

    Removing bad fonts, adding new styles and cover images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,22 @@
 (function($){
 	"use strict";
 	$(document).ready(function(){
+        $(".article .post").each(function() {
+
+        	if ( $( this ).find("section.text p:first").has("img").length ) {
+        		var $image = $( this ).find("section.text p:first img");
+
+        		if ( $image.attr("alt") == "cover image") {
+        			console.log("Cover image found");
+
+        			$( this ).find("section.image").find("img").css("background-image", 'url(' + $image.attr("src") + ')');
+        			$( this ).find("section.image").show();
+
+        			$image.remove();
+        		}
+        	}
+        });
+
         var $container = $('.articles');
         $container.masonry({
             itemSelector: '.article'
