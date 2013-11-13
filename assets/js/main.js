723bd415e5f090cbe1b1aa7878e508249d7abc31
Jason Millward <jason@jcode.me>
2013-11-13 04:26:57 +0000

    Updating theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,9 @@
-
-var container = document.querySelector('.articles');
-var msnry = new Masonry( container, {
-	itemSelector: '.article'
-});
-
 (function($){
 	"use strict";
 	$(document).ready(function(){
-		$(".tags p").each(function() {
-			if ( !$( this ).text().trim().length) {
-				$( this ).parent().remove();
-			} 
-		});	
+        var $container = $('.articles');
+        $container.masonry({
+            itemSelector: '.article'
+        });
 	});
 }(jQuery));
