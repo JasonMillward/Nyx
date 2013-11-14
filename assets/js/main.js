a35c1823a994f91fb32f46fe902ba7f79ffa2758
Jason Millward <jason@jcode.me>
2013-11-14 04:24:44 +0000

    Cleaning up js

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,6 @@
 
     $(document).ready(function(){
         var $container = $('.articles');
-        var pages = jQuery(".pagination span").html().split(" of ");
 
         cover_images();
 
@@ -24,19 +23,21 @@
             itemSelector: '.article'
         });
 
-        $('.articles').infinitescroll({
-            navSelector:    ".pagination",
-            nextSelector:   ".pagination a",
-            itemSelector:   ".article",
-            debug:          false,
-            dataType:       'html',
-            maxPage:        parseInt(pages[1]),
-        }, function(newElements, data, url){
-            cover_images();
-            setTimeout(function(){
-                $('.articles').masonry('reloadItems').masonry('layout');
-            }, 500);
-        });
+        if ( $(".pagination span").length ) {
+            var pages = $(".pagination span").html().split(" of ");
+            $('.articles').infinitescroll({
+                navSelector:    ".pagination",
+                nextSelector:   ".pagination a",
+                itemSelector:   ".article",
+                debug:          false,
+                dataType:       'html',
+                maxPage:        parseInt(pages[1]),
+            }, function(newElements, data, url){
+                cover_images();
+                setTimeout(function(){
+                    $('.articles').masonry('reloadItems').masonry('layout');
+                }, 500);
+            });
+        }
     });
-
 }(jQuery));
