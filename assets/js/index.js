ef44a18420e817d9fd0e5fb9861279be3f83621c
Jason Millward <jason@jcode.me>
2014-08-04 17:09:46 +0930

    Updated to be Nyx

---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+(function($){
+    "use strict";
+
+    var cover_images = function() {
+        $(".article .post").each(function() {
+            if ( $( this ).find("section.text p:first").has("img").length ) {
+                var $image = $( this ).find("section.text p:first img");
+                if ( $image.attr("alt") == "cover image") {
+                    $( this ).find("section.image").find("img").css("background-image", 'url(' + $image.attr("src") + ')');
+                    $( this ).find("section.image").show();
+                    $image.remove();
+                }
+            }
+        });
+    }
+    $(document).ready(function(){
+        var $container = $('.articles');
+        cover_images();
+        $container.masonry({
+            itemSelector: '.article'
+        });
+        if ( $(".pagination span").length ) {
+            var pages = $(".pagination span").html().split(" of ");
+            $('.articles').infinitescroll({
+                navSelector:    ".pagination",
+                nextSelector:   ".pagination a",
+                itemSelector:   ".article",
+                debug:          false,
+                dataType:       'html',
+                maxPage:        parseInt(pages[1]),
+            }, function(newElements, data, url){
+                cover_images();
+                setTimeout(function(){
+                    $('.articles').masonry('reloadItems').masonry('layout');
+                }, 500);
+            });
+        }
+    });
+}(jQuery));
